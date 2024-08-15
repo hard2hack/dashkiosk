003173a3da7dc51377e1d021e2f1e2343560a991
Federico Toscano <federico@dolen.org>
2024-08-15 15:30:42 +0100

    Removed technical info when OSDing

--- app/scripts/receiver/osd.js
+++ app/scripts/receiver/osd.js
@@ -8,20 +8,6 @@ module.exports = (function(window, undefined) {
         function(osd) { osd.classList.remove('show'); });
     },
     show: function(text) {
-      document.querySelector('.osd.technical').innerHTML = (
-          '<p>clientWidth/Height: ' +
-            (window.document.documentElement.clientWidth || '???') + '×' +
-            (window.document.documentElement.clientHeight || '???') +
-            '</p><p>' +
-            '<p>innerWidth/Height: ' +
-            (window.innerWidth || '???') + '×' +
-            (window.innerHeight || '???') +
-            '</p><p>' +
-            '<p>screenWidth/Height: ' +
-            (window.screen.width || '???') + '×' +
-            (window.screen.height || '???') +
-          '</p>'
-      );
       document.querySelector('.osd.text').textContent = text;
       Array.prototype.forEach.call(
         document.querySelectorAll('.osd'),
